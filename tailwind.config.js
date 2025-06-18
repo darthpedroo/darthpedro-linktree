ae5faa3718a9bc763d0ef0178dc4a78b32390b12
darthpedroo <pedrovillarino06@gmail.com>
2025-06-18 18:17:16 -0300

    added tailwind as a package

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        xs: '250px',
+      },
+    },
+  },
+  content: ['./**/*.{html,js}'], // Asegurate que incluya tus archivos
+};
